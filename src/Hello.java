0fa86999bf5fd6ac9b8c71c340a7a539c1a7d7f3
Igor Lebedyuk <ilebeduyk@gmail.com>
2018-04-01 12:28:00 +0300

    Revert: fourth commit

--- src/Hello.java
+++ src/Hello.java
@@ -4,8 +4,5 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hello1");
-        System.out.println("Hello2");
-        System.out.println("Hello3");
     }
 }
