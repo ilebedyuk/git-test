00114da01fc0526a80432892fc205451456c9781
Igor Lebedyuk <ilebeduyk@gmail.com>
2018-04-01 12:29:40 +0300

    1

--- src/Hello.java
+++ src/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello12");
+        System.out.println("Hello13");
     }
 }
