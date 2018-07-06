3a1051ade42b66b14ece8a37d33cda61a22c9673
Igor Lebedyuk <ilebeduyk@gmail.com>
2018-07-06 13:19:40 +0300

    delete souts

--- src/Hello.java
+++ src/Hello.java
@@ -4,12 +4,5 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Mster");
-        System.out.println("Mster1");
-        System.out.println("Hello12");
-        System.out.println("Hello13");
-        System.out.println("dev1");
-        System.out.println("Mster");
-        System.out.println("Mster1");
     }
 }
