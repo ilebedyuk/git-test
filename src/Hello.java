14e2862d6b52113fa544e3fb143465bdff33911f
Igor Lebedyuk <ilebeduyk@gmail.com>
2018-07-06 13:31:06 +0300

    hello2

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello2");
         System.out.println("master1");
         System.out.println("test1");
         System.out.println("test2");
