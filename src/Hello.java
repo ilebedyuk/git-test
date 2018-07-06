acc05f5424aa4099e02021235a4e9dffc72dfe60
Igor Lebedyuk <ilebeduyk@gmail.com>
2018-07-06 13:30:00 +0300

    delete hello

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,6 @@
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello2");
         System.out.println("master1");
         System.out.println("test1");
         System.out.println("test2");
